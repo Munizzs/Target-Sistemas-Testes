b88ead48df40a2db83bb40cce1cb3ae3d25795c8
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-09-06 17:51:51 -0300

    Teste 1

---
+++ src/valorSoma/App.java
@@ -0,0 +1,14 @@
+package valorSoma;
+
+public class App {
+    public static void main(String[] args) {
+        int indice = 13, soma = 0, k=0;
+
+        while(k<indice){
+            k++;
+            soma+=k;
+        }
+        System.out.println(soma);
+    }
+
+}
