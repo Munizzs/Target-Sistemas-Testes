d93ccc710d391bf6a2b51fa8977208e62a86f91f
Matheus Muniz <matheusmuniz0810@gmail.com>
2024-09-07 12:24:53 -0300

    Melhoria do Teste 2

--- src/fibonacciNumero/App.java
+++ src/fibonacciNumero/App.java
@@ -2,6 +2,10 @@ package fibonacciNumero;
 
 import java.util.Scanner;
 
+/*2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores
+(exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), escreva um programa na linguagem que desejar onde, informado um número,
+ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.*/
+
 public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -12,9 +16,9 @@ public class App {
         Fibonacci fibonacci = new Fibonacci(numero);
 
         if(fibonacci.ehFibonacci()){
-            System.out.println("O numero "+numero+" é um Fibonacci");
+            System.out.println(fibonacci+" pertence à sequência de Fibonacci.");
         }else{
-            System.out.println("O numero "+numero+" não é um Fibonacci");
+            System.out.println(fibonacci+" não pertence à sequência de Fibonacci.");
         }
     }
 }
--- src/fibonacciNumero/Fibonacci.java
+++ src/fibonacciNumero/Fibonacci.java
@@ -8,16 +8,25 @@ public class Fibonacci {
     }
 
     public boolean ehFibonacci(){
-        int numeroAtual = 1, ultimoNumero = 0;
-        while(numeroAtual<=this.numero){
-            int temporario = numeroAtual;
-            numeroAtual = ultimoNumero + numeroAtual;
-            ultimoNumero = temporario;
+        if (numero == 0 || numero == 1) {
+            return true;
+        }
 
+        int numeroAtual = 1, ultimoNumero = 0;
+
+        while(numeroAtual<=this.numero){
             if(numeroAtual == this.numero){
                 return true;
             }
+            int temporario = numeroAtual;
+            numeroAtual = ultimoNumero + numeroAtual;
+            ultimoNumero = temporario;
         }
         return false;
+    }
+
+    @Override
+    public String toString() {
+        return "O numero "+numero;
     }
 }
